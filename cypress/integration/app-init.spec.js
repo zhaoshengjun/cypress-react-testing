7bb0c38dc96e52fc91896266774abab3511776e6
zhaoshengjun <zhao.shengjun@gmail.com>
2018-05-11 23:43:46 +1000

    refactor fixture

--- cypress/integration/app-init.spec.js
+++ cypress/integration/app-init.spec.js
@@ -2,9 +2,7 @@ describe('App initialization', () => {
   it('Displays todos from API on load', () => {
     // stub server
     cy.server();
-    cy.fixture('todos').then((todos) => {
-      cy.route('GET', '/api/todos', todos);
-    })
+    cy.route('GET', '/api/todos', 'fixture:todos');
 
     cy.visit('/');
     cy.get('.todo-list li').should('have.length', 4)
