04d15df10007aff06fbf813226728418d804edc5
Sam Zhao <zhao.shengjun@gmail.com>
2018-05-11 13:55:30 +1000

    init commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Testing React App with Cypress
+
+This repo shows how to testing React app using Cypress.
+
+## Install
+
+```bash
+yarn
+yarn run dev
+```
+
+After installation finished, you can open `localhost:3030` to view the app.
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: './src/index.html',
+  filename: 'index.html',
+  inject: 'body'
+});
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    path: path.join(__dirname, 'build'),
+    filename: 'app.bundle.js'
+  },
+  module: {
+    rules: [
+      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+        exclude: /node_modules/
+      }
+    ]
+  },
+  devtool: 'source-map',
+  plugins: [HtmlWebpackPluginConfig]
+};
