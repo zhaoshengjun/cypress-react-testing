fb979b30ab0f3e339a36435ef0630d36114fc7cd
zhaoshengjun <zhao.shengjun@gmail.com>
2018-05-11 23:56:00 +1000

    update form input tests

--- cypress/integration/form-input.spec.js
+++ cypress/integration/form-input.spec.js
@@ -1,14 +1,17 @@
-describe('Form input', () => {
+describe("Form input", () => {
   beforeEach(() => {
-    cy.visit('/');
-  })
-  it('visits the app', () => {
-    cy.focused().should('have.class', 'new-todo')
-  })
+    cy.seedAndVisit();
+  });
+  it("visits the app", () => {
+    cy.focused().should("have.class", "new-todo");
+  });
 
-  it('Accepts input', () => {
-    const typedText = 'New todo'
+  it("Accepts input", () => {
+    const typedText = "New todo";
 
-    cy.get('.new-todo').type(typedText).should('have.value', typedText);
-  })
-})
+    cy
+      .get(".new-todo")
+      .type(typedText)
+      .should("have.value", typedText);
+  });
+});
