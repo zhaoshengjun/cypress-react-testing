a943668675b68e46eca72836dd1c69ff6e999484
zhaoshengjun <zhao.shengjun@gmail.com>
2018-05-11 23:35:55 +1000

    use stub to fake api call

--- cypress/integration/app-init.spec.js
+++ cypress/integration/app-init.spec.js
@@ -1,5 +1,14 @@
 describe('App initialization', () => {
   it('Displays todos from API on load', () => {
+    // stub server
+    cy.server();
+    cy.route('GET', '/api/todos', [
+      { id: 1, name: 'One', isCompleted: false },
+      { id: 2, name: 'Two', isCompleted: false },
+      { id: 3, name: 'Three', isCompleted: false },
+      { id: 4, name: 'Four', isCompleted: false }
+    ]);
+
     cy.visit('/');
     cy.get('.todo-list li').should('have.length', 4)
   })
