90b7170f3ff657deedc7beb9d492d3cecc35c6f9
zhaoshengjun <zhao.shengjun@gmail.com>
2018-05-11 23:26:11 +1000

    type test

--- cypress/integration/form-input.spec.js
+++ cypress/integration/form-input.spec.js
@@ -3,4 +3,10 @@ describe('Form input', () => {
     cy.visit('/');
     cy.focused().should('have.class', 'new-todo')
   })
+
+  it.only('Accepts input', () => {
+    const typedText = 'New todo'
+    cy.visit('/')
+    cy.get('.new-todo').type(typedText).should('have.value', typedText)
+  })
 })
