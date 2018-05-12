a876f6cc05912718cf02e9eb1a35babd016e3200
zhaoshengjun <zhao.shengjun@gmail.com>
2018-05-12 17:07:58 +1000

    test add todo

---
+++ cypress/integration/form-submission.spec.js
@@ -0,0 +1,21 @@
+describe("Form submission", () => {
+  it("Adds a new todo item", () => {
+    const newTodo = "Buy Milk";
+    cy.server();
+    cy
+      .route({
+        method: "post",
+        url: "/api/todos",
+        response: { id: 123, name: newTodo, isComplete: false }
+      })
+      .as("save");
+
+    cy.seedAndVisit();
+    cy
+      .get(".new-todo")
+      .type(newTodo)
+      .type("{enter}");
+    cy.wait("@save");
+    cy.get(".todo-list li").should("have.length", 5);
+  });
+});
