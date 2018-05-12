95c3300fffbc0058988a07e1fc5d00366ff2b2fe
zhaoshengjun <zhao.shengjun@gmail.com>
2018-05-12 22:43:33 +1000

    delete todos

--- cypress/integration/smoke-test.spec.js
+++ cypress/integration/smoke-test.spec.js
@@ -15,4 +15,31 @@ describe("Smoke tests", () => {
       cy.get(".todo-list li").should("have.length", 1);
     });
   });
+
+  context("With todos", () => {
+    beforeEach(() => {
+      cy.fixture("todos").then(todos => {
+        cy.request("POST", "/api/todos/bulkload", { todos });
+      });
+      cy.server();
+      cy.route("GET", "/api/todos").as("load");
+      cy.visit("/");
+      cy.wait("@load");
+    });
+
+    it.only("Delete todos", () => {
+      cy.route("DELETE", "/api/todos/*").as("delete");
+      cy
+        .get(".todo-list li")
+        .each($el => {
+          cy
+            .wrap($el)
+            .find(".destroy")
+            .invoke("show")
+            .click();
+          cy.wait("@delete");
+        })
+        .should("have.length", 0);
+    });
+  });
 });
