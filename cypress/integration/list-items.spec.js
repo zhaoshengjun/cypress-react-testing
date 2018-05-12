641b6b65c64f3e0bfb375f4a4018737010c61e17
zhaoshengjun <zhao.shengjun@gmail.com>
2018-05-12 17:19:55 +1000

    force click invisible button test

---
+++ cypress/integration/list-items.spec.js
@@ -0,0 +1,19 @@
+describe("List item behavior", () => {
+  it("Deletes an item", () => {
+    cy.server();
+    cy
+      .route({
+        method: "DELETE",
+        url: "/api/todos/*",
+        response: {}
+      })
+      .as("delete");
+
+    cy.seedAndVisit();
+    cy
+      .get(".todo-list li")
+      .first()
+      .find(".destroy")
+      .click({ force: true });
+  });
+});
